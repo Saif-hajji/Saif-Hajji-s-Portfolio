099b44bf9c441ae22adf63e85775ee5f49cb5416
saif-hajji <saifedine.hajji@gmail.com>
2019-11-17 18:01:32 +0100

    Updates

---
+++ precache-manifest.72924c70097650aedda6ede2895d5ba7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69942d830472abdaf372cb36040ec8e1",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "a76322ea1a91f38aad3e",
+    "url": "/my-app/static/css/2.04aae6c4.chunk.css"
+  },
+  {
+    "revision": "8817464b70c4fcd80463",
+    "url": "/my-app/static/css/main.e8c24495.chunk.css"
+  },
+  {
+    "revision": "a76322ea1a91f38aad3e",
+    "url": "/my-app/static/js/2.6ff1da82.chunk.js"
+  },
+  {
+    "revision": "8817464b70c4fcd80463",
+    "url": "/my-app/static/js/main.ef5c201e.chunk.js"
+  },
+  {
+    "revision": "e08cb7cedae51d335249",
+    "url": "/my-app/static/js/runtime-main.d6ec55de.js"
+  }
+]);
